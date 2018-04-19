5de63434341a1b03cddd6c04f60711163ed7ce86
rajita1 <35609181+rajita1@users.noreply.github.com>
2018-04-19 08:50:47 -0700

    Update 2-create-accounts.sh
    Added public keys for 'rajita' account creation

--- scripts/2-create-accounts.sh
+++ scripts/2-create-accounts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cleos create account eosio alex <TODO> <TODO>
-cleos create account eosio rajita <TODO> <TODO>
+cleos create account eosio rajita EOS84bsvjREv6YVSLuiZEBu8BKnTQzyCkMLETs4vf9wT8HgQjpnd2 EOS51s8v4eWH1tPMXW9fUek9ZnRMHADBeuLSAzup6VU2SokuhdVJw
 cleos create account eosio michael <TODO> <TODO>
